7058d45ccbe65e3b2bd67647d20b6cb2cf355d79
Jens Lindström <jens.swedish@gmail.com>
2018-08-16 18:20:39 +0200

    Updated service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('472').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+          '/precomp.js',
+          '/favicon.ico'
+        ]);
+      })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+
+    console.log(event.request.url);
+    
+});
