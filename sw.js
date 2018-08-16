52552c2bf65ce7753dbdce79a31f644bf081615b
Jens Lindström <jens.swedish@gmail.com>
2018-08-16 18:25:01 +0200

    Updated sw.js

--- sw.js
+++ sw.js
@@ -18,4 +18,14 @@ self.addEventListener('fetch', function(event) {
 
     console.log(event.request.url);
     
+    event.respondWith(
+    
+    caches.match(event.request).then(function(response) {
+    
+    return response || fetch(event.request);
+    
+    })
+    
+    );
+    
 });
